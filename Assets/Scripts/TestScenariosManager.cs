54a2fe6e76b314782629f30fbf8d30dc391e4478
Vinicius Queiroz <vinicius.queiroz@tjpa.jus.br>
2023-06-21 19:21:41 -0300

    Configura script dos cenários de teste

--- Assets/Scripts/TestScenariosManager.cs
+++ Assets/Scripts/TestScenariosManager.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class TestScenariosManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    string _serverIp = "";
+    string _dataset = "";
+    string _eixoX = "";
+    string _eixoY = "";
+    string _cor = "";
+    string _subVis= "";
+
+    public RequisitionManager _requisitionManager;
+    public SettingsManager _settingsManager;
+
+    public void ConfiguraCenario01()
     {
-        
+        _serverIp = "127.0.0.1";
+        _dataset = "carros_teste";
+        _eixoX = "MARCA";
+        _eixoY = "ANO";
+        _cor = "ORIGEM";
+        _subVis = "TIPO";
+
+        RealizaRequisicao();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ConfiguraCenario02()
     {
-        
+        _serverIp = "127.0.0.1";
+        _dataset = "carros_teste";
+        _eixoX = "MARCA";
+        _eixoY = "NRCILINDROS";
+        _cor = "TIPO";
+        _subVis = "COMBUSTIVEL";
+
+        RealizaRequisicao();
     }
+
+    public void RealizaRequisicao()
+    {
+        _requisitionManager.SetEnderecoServidor(_serverIp);
+        _requisitionManager.GetDatasetPorNomeAtributosPreenchidos(_dataset);
+        _settingsManager.SetQtdBarras(6);
+    }
+
+    public void DefineAtributosSelecionados()
+    {
+        DatasetManager.SetNomeDataset(_dataset);
+        DatasetManager.SetNomeEixoX(_eixoX);
+        DatasetManager.SetNomeEixoY(_eixoY);
+        DatasetManager.SetNomeCor(_cor);
+        DatasetManager.SetNomeAtributoSubVisualizacao(_subVis);
+    }
+
+
+
+
+    //TODO: Aplicar essa configurações nos métodos para verificar se a interface corresponde!
 }
